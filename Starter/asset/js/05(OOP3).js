a41077f642f0aed04d00a1a8b9f107e40a7515ce
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-02-15 21:10:39 +0300

    oop3

--- Starter/asset/js/05(OOP3).js
+++ Starter/asset/js/05(OOP3).js
@@ -1,5 +1,16 @@
 // 1. Create a Person_ES6 Class 
+class Person_ES6 
+{
+    contracture(firstName, lastName)
+    {
+        this.firstName = firstName ; 
+        this.lastName = lastName ; 
+    }
+    greeting(){
+        return `Hi, ${this.firstName}, ${this.lastName}` ;
+    }
 
+}
 
 
 //2. Create the constructor that initialize firstName and lastName
@@ -9,8 +20,11 @@
 
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
+const Bran = new Person_ES6()
 
+Bran.firstName = "Bran"
+Bran.lastName = "Stark"
 
-
+// console.log(Bran);
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
