48faa5fc8031e9c776d769e79cf1828f09f08c3a
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-02-15 11:51:59 +0300

    function borrow

---
+++ Starter/asset/js/04(function_borrowing).js
@@ -0,0 +1,67 @@
+//DOM Load 
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    usingCall();
+    usingApply();
+    usingBind();
+
+
+});
+
+//Brad Person Object 
+const Brad = {
+    firstName: "Brad",
+    lastName: "Bansely",
+    fullName: function(ui_place, message) {
+
+        ui_place.innerHTML = `${message} ${this.firstName} ${this.lastName}`;
+
+    }
+}
+
+function usingCall() {
+
+    //Cerscy Person Object 
+    const Cerscy = {
+        firstName: "Cerscy ",
+        lastName: "Lanster",
+    }
+
+    //1. Borrow fullName using call
+    Brad.fullName.call(Cerscy, call_demo, "Hi I am") 
+    //2. Pass call_demo[as ui_place] , Hi I am ,[message]
+    
+}
+
+function usingApply() {
+
+    //Jon Person Object 
+    const Jon = {
+        firstName: "Jon",
+        lastName: "Snow",
+    }
+
+    //1. Borrow fullName using apply
+    Brad.fullName.apply(Jon, [apply_demo ,'Hi I ma'])
+    //2. Pass apply_demo[as ui_place] and Hi I am ,[as message] as array
+
+
+}
+
+function usingBind() {
+
+    //Daenerys Person Object 
+    const Daenerys = {
+        firstName: "Daenerys",
+        lastName: "Targaryen",
+    }
+
+    //1. Borrow fullName using bind
+    
+    let newFunc = Brad.fullName.bind(Daenerys, bind_demo, 'Hi I am')
+    newFunc();
+    //2. Pass bind_demo[as ui_place] , Hi I am ,[as message]
+
+
+}
