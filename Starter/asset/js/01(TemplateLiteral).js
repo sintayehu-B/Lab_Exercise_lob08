c17eed6e1b8f1e4c7d1cf6d8cf8adee6228baf38
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-02-15 11:49:24 +0300

    Templeatelitearal

---
+++ Starter/asset/js/01(TemplateLiteral).js
@@ -0,0 +1,50 @@
+// //DOM Load 
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     templateStringConcatenation();
+
+//     displayTemplateExpression();
+
+
+// })
+
+
+// function templateStringConcatenation() {
+
+//     // A Student Object 
+//     const student = { name: "Abebe", age: 21 }
+
+//     //String to Display 
+//     // const fullIntro = "My Name is " + `${student.name}` + " and i am " + `${student.age}` + " Years Old"
+
+//     //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
+//     const fullIntroTemplate = `"My Name is " + ${student.name}` + `" and i am " + ${student.age}` + " Years Old"
+
+//     //3. Display it [change it to fullIntroTemplate]
+//     templateStringConcatenation.innerHTML = fullIntroTemplate;
+//     // templateConcatenationResult.innerHTML = fullIntro;
+
+// }
+
+// function displayTemplateExpression() {
+
+
+//     //String to Display 
+//     const result = "The Sum of the Numbers is  : " + sum(1, 2)
+
+//     //1. Create a variable called result1 and Store the same string displayed above with template literal
+//     const result1 =  `"The Sum of the Numbers is  : " + ${sum(1, 2)}`;
+
+//     //3. Display it [change it to result1]
+//     templateExpression.innerHTML = result;
+//     templateExpression.innerHTML = result1;
+
+
+
+// }
+
+// function sum(x, y) {
+
+//     return x + y;
+
+// }
