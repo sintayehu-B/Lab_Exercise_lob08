acd57f4d2703609e032cbe953b670b828535058e
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-02-15 12:04:20 +0300

    oop1

--- Starter/asset/js/05(OOP1).js
+++ Starter/asset/js/05(OOP1).js
@@ -2,17 +2,21 @@
 function Person(firstName, lastName) {
 
     // 1. set the firstName and lastName using this keyword
-    
+    this.firstName = firstName;
+    this.lastName = lastName;
 
     //2. Create a greeting function returns [use this keyword to get the first and last name]
     //  ==> Hi , firstName lastName
-
+    this.greeting = function(){
+        return "hi " + this.firstName + this.lastName
+    }
 
 
 }
 
 //3. Create Jon Whose full name is ["Jon", "Snow"] Object using new keyword [new ConstructorFun()]
-
+const jon = new Person("Jon","Snow");
+console.log(jon);
 
 //4. Display the greeting on Jon Object [remove the string when you have the object]
-Obj_C1.innerHTML = "Jon.greeting()";
+Obj_C1.innerHTML = jon.greeting();
